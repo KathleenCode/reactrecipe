8dfb012956f7ff5dbff05d214998dd2738caede5
KathleenCode <kathleenbonaventure@gmail.com>
2023-12-12 09:15:57 +0000

    some changes added

--- src/components/Back.jsx
+++ src/components/Back.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 export default function Back() {
   return (
     <>
-    <Link to="/" style={{padding: "1rem 1.7rem", border: "1px dashed #F8FF95", display: "flex", justifyContent: "start", 
+    <div style={{display: "flex", justifyContent: "center"}}>
+    <Link to="/" style={{padding: "1rem 1.7rem", border: "1px dashed #F8FF95", display: "flex", justifyContent: "center",
     // backgroundColor: "#FEFFAC", 
     backgroundImage: "linear-gradient(#F8FF95, #EBE76C, #FFFD8C)",
     width: "10vw", margin: "1rem", borderRadius: "10px", fontWeight: "bold"}}>PREVIOUS RECIPE</Link>
+   </div>
     </>
   )
 }
